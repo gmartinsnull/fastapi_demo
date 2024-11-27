a2f856b452328da7f0b085aa77fb8ce63dcd8d52
gmartinsnull <gmartins.dev@gmail.com>
2024-11-27 07:32:12 -0800

    add print in unit test

--- tests/unit_tests/test_main.py
+++ tests/unit_tests/test_main.py
@@ -5,18 +5,23 @@ from app.main import app  # Adjust this import based on your file structure
 
 client = TestClient(app)
 
+
 def test_root_endpoint():
     """
     Test the root endpoint '/'
     """
+    print("Testing root endpoint")
     response = client.get("/")
     assert response.status_code == 200
-    assert "Welcome to the Iris Species Prediction API!" in response.json()["message"]
+    assert "Welcome to the Iris Species Prediction API!" in response.json()[
+        "message"]
+
 
 def test_predict_endpoint():
     """
     Test the predict endpoint '/predict/'
     """
+    print("Testing predict endpoint")
     response = client.get("/predict/")
     assert response.status_code == 200
     assert response.json() == {"message": "This is the predict endpoint."}
