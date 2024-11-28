122462260d8ea19dd84deb9b3c21975a57c3b9e5
gmartinsnull <gmartins.dev@gmail.com>
2024-11-28 07:35:24 -0800

    add echo in bash

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e  # Exit immediately if any command fails
+echo "Running unit tests..."  # This should show up in Vercel logs
 pip install -r requirements.txt
 pytest tests/unit_tests/ -v -s
