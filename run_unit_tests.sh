73fdfbe92dc311855021f333a101ad212deae0cb
gmartinsnull <gmartins.dev@gmail.com>
2024-11-28 07:47:07 -0800

    update bash with python3

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -2,5 +2,6 @@
 set -e  # Exit immediately if any command fails
 echo "Running unit tests..."  # This should show up in Vercel logs
 # Install required packages
-python -m pip install -r requirements.txt
-pytest tests/unit_tests/ -v -s
+python3 --version
+python3 -m pip install -r requirements.txt
+python3 -m pytest tests/unit_tests/ -v -s
