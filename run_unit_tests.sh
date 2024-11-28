4d034b80023cb611b2a2df0b8ca20ebaf3094ec9
gmartinsnull <gmartins.dev@gmail.com>
2024-11-28 08:01:21 -0800

    rollback to functional state

--- run_unit_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e  # Exit immediately if any command fails
-echo "Running unit tests..."  # This should show up in Vercel logs
-# Install required packages
-python3 -m pip install -r requirements.txt
-python3 -m pytest tests/unit_tests/ -v -s
