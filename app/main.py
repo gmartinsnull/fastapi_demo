e5f22a85a76b9d88c50fac6117faddf5455633ef
gmartinsnull <gmartins.dev@gmail.com>
2024-11-29 07:29:58 -0800

    add include root route

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ Root endpoint returns the app description
 """
 
 from fastapi import FastAPI
-from .routes import test_route_v1
+from .routes import test_route_v1, root_route_v1
 from .routes.middleware import AuthMiddleware
 
 
@@ -13,7 +13,7 @@ def create_app():
     create FastAPI app
     """
     fastapi = FastAPI()
-    fastapi.include_router(test_route_v1.router)
+    fastapi.include_router(root_route_v1.router)
     fastapi.include_router(test_route_v1.router, prefix="/v1")
     return fastapi
 
